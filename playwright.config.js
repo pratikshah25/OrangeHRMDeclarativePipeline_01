9d6a42e4ebaf6c08d0e47ab0cec381429d2aa328
unknown <itspratikshah25.1990@gmail.com>
2026-08-31 19:37:08 +0530

    Created new Orange HRM Project

---
+++ playwright.config.js
@@ -0,0 +1,49 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+    testDir : './tests',
+
+    fullyParallel : true,
+
+    workers : 2,
+
+    reporter : [
+        ['list'],['allure-playwright',{
+        resultsDir : 'allure-results'
+    }]
+],
+
+    use :
+    {
+        headless : false,
+        screenshot : 'on',
+        video : 'on',
+        trace : 'on'
+    },
+
+    projects : 
+    [
+        {
+            name : 'chromium',
+            use:
+            {
+                browserName : 'chromium'
+            }
+        },
+        {
+            name : 'firefox',
+            use :
+            {
+                browserName : 'firefox'
+            }
+        },
+        {
+            name : 'webkit',
+            use :
+            {
+                browserName : 'webkit'
+            }
+        }
+
+    ]
+});
